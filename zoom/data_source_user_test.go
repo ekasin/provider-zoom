c3b06be433185ea589c6c511419a7bbe6037cc9e
Namrata Chaurasia <50015194+chaurasia-namrata@users.noreply.github.com>
2021-05-07 16:29:54 +0530

    Update data_source_user_test.go

--- zoom/data_source_user_test.go
+++ zoom/data_source_user_test.go
@@ -26,7 +26,43 @@ func testAccUserDataSourceConfig() string {
 	return fmt.Sprintf(`	  
 	resource "zoom_user" "user1" {
 		email        = "ui17co15@iitsurat.ac.in"
+		first_name   = "ekansh"package zoom
+
+import (
+	"fmt"
+	"testing"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccUserDataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccUserDataSourceConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.zoom_user.user1", "id", "ui17ec38@iiitsurat.ac.in"),
+				),
+			},
+		},
+	})
+}
+
+func testAccUserDataSourceConfig() string {
+	return fmt.Sprintf(`	  
+	resource "zoom_user" "user1" {
+		email        = "ui17co15@iitsurat.ac.in"
 		first_name   = "ekansh"
+		last_name    = "singh"
+		type         =  1
+	  }
+	data "zoom_user" "user1" {
+		id = "ui17ec38@iiitsurat.ac.in"
+	}
+	`)
+}
 		last_name    = "singh"
 		type         =  1
 	  }
