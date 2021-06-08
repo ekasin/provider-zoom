7520a47b1e882c0f34f0cbf3428a4962a25afc52
ekasin <42174827+ekasin@users.noreply.github.com>
2021-06-08 17:47:05 +0530

    Update data_source_user.go

--- zoom/data_source_user.go
+++ zoom/data_source_user.go
@@ -1,129 +1,68 @@
 package zoom
 
 import (
-	"fmt"
-	"strings"
-	"terraform-provider-zoom/client"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+   "strings"
+   "fmt"
+   "terraform-provider-zoom/client"
+   "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func dataSourceUser() *schema.Resource {
-	return &schema.Resource{
-		Read: dataSourceUserRead,
-		Schema: map[string]*schema.Schema{
-			"id": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
+  return &schema.Resource{
+    Read: dataSourceUserRead,
+    Schema: map[string]*schema.Schema{
+	  "first_name": &schema.Schema{
+		  Type:     schema.TypeString,
+		  Computed: true,
+	  },
+	  "last_name": &schema.Schema{
+        	Type:     schema.TypeString,
+        	Computed: true,
+      	  },
+	  "email": &schema.Schema{
+        	Type:     schema.TypeString,
+        	Computed: true,
+      	  },
+	  "status": &schema.Schema{
+		Type:        schema.TypeString,
+		Computed :   true,
+	   },
+	   "id": &schema.Schema{
+        	Type:     schema.TypeString,
+        	Required: true,
+      	   },
+      	   "license_type": &schema.Schema{
+        	Type:     schema.TypeInt,
+        	Computed: true,
+           },
+      	   "pmi": &schema.Schema{
+		Type:        schema.TypeInt,
+		Computed:    true,
+	    },
+      	    "role_name": &schema.Schema{
+		Type:        schema.TypeString,
+		Computed:    true,
 			},
-			"email": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"first_name": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"last_name": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"type": &schema.Schema{
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"role_name": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"pmi": &schema.Schema{
-				Type:     schema.TypeInt,
-				Computed: true,
-				Optional: true,
-			},
-			"use_pmi": &schema.Schema{
-				Type:     schema.TypeBool,
-				Computed: true,
-				Optional: true,
-			},
-			"timezone": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"verified": &schema.Schema{
-				Type:     schema.TypeInt,
-				Computed: true,
-				Optional: true,
-			},
-			"dept": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"host_key": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"cms_user_id": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"jid": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"account_id": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"language": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"phone_country": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"phone_number": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"status": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"job_title": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"location": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-			"role_id": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
-			},
-		},
-	}
+            "department": &schema.Schema{
+		Type:        schema.TypeString,
+		Computed:    true,
+	     },
+	     "job_title": &schema.Schema{
+		Type:        schema.TypeString,
+		Computed:    true,
+	      },
+	      "location": &schema.Schema{
+		Type:        schema.TypeString,
+		Computed:    true,
+	      },
+    },
+  }
 }
 
 func dataSourceUserRead(d *schema.ResourceData, m interface{}) error {
 	apiClient := m.(*client.Client)
-	userId := d.Get("id").(string)
-	user, err := apiClient.GetItem(userId)
+  	userId := d.Get("id").(string)
+  	user, err := apiClient.GetItem(userId)
 	if err != nil {
 		if strings.Contains(err.Error(), "not found") {
 			d.SetId("")
@@ -131,29 +70,18 @@ func dataSourceUserRead(d *schema.ResourceData, m interface{}) error {
 			return fmt.Errorf("error finding Item with ID %s", userId)
 		}
 	}
-
-	d.SetId(user.Email)
+  	d.SetId(user.Email)
 	d.Set("email", user.Email)
-	d.Set("id", user.Id)
 	d.Set("first_name", user.FirstName)
 	d.Set("last_name", user.LastName)
-	d.Set("type", user.Type)
-	d.Set("role_name", user.RoleName)
-	d.Set("pmi", user.Pmi)
-	d.Set("use_pmi", user.UsePmi)
-	d.Set("timezone", user.TimeZone)
-	d.Set("verified", user.Verified)
-	d.Set("dept", user.Dept)
-	d.Set("host_key", user.HostKey)
-	d.Set("cms_user_id", user.CmsUserId)
-	d.Set("jid", user.Jid)
-	d.Set("account_id", user.AccountId)
-	d.Set("language", user.Language)
-	d.Set("phone_country", user.PhoneCountry)
-	d.Set("phone_number", user.PhoneNumber)
-	d.Set("status", user.Status)
+  	d.Set("license_type",user.Type)
+  	d.Set("pmi",user.Pmi)
+  	d.Set("status",user.Status)
+  	d.Set("role_name",user.RoleName)
+  	d.Set("department",user.Department)
 	d.Set("job_title", user.JobTitle)
 	d.Set("location", user.Location)
-	d.Set("role_id", user.RoleId)
-	return nil
+  	return nil
 }
+  
+  
